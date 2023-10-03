6c6c33071319e3795ee79da3c7f65b7a53f8a6b9
m4rcosmatos <m4rcosff@gmail.com>
2023-10-03 16:42:04 -0300

    Quiz

---
+++ src/components/Question.jsx
@@ -0,0 +1,27 @@
+import Option from './Option';
+
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+
+import './Question.css'
+
+const Question = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+    const currentQuestion = quizState.questions[quizState.currentQuestion];
+  return (
+    <div id='question'>
+        <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
+        <h2>{currentQuestion.question}</h2>
+        <div id="options-container">
+            {currentQuestion.options.map((option) =>(
+                <Option option = {option} key={option}/>
+            ))}
+        </div>
+        <button onClick={()=> dispatch({type: "CHAGE_QUESTION"})} >
+            Continuar
+        </button>
+    </div>
+  )
+}
+
+export default Question
